9c121318456a409202f33cea0bac66f088f754e8
istrelkina <strelaweb@gmail.com>
2020-02-23 16:50:00 +1000

    added popup

--- app/js/main.js
+++ app/js/main.js
@@ -45,7 +45,17 @@ $(function(){
     });
 
 
+    $('.product-one__tabs .tab').on('click', function(event) {
+        var id = $(this).attr('data-id');
+        $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#'+id).addClass('active-tab').fadeIn();
+        return false;
+    });
 
-	var mixer = mixitup('.products__inner-box');
+
+
+	var mixer = mixitup('.products__inner-box'); //всегда должен идти в конце!
 
 });
